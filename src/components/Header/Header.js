bb9780b54536a89cd2c9df969e1be81bd6e5fa31
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2021-01-17 14:26:26 -0300

    :construction: feat: Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import { Container, Grid } from "@material-ui/core";
+
+import { HeaderWrapper } from "./styles";
+
+import Nav from "../Nav/Nav";
+import Brand from "../Brand/Brand";
+
+const Header = () => {
+  return (
+    <HeaderWrapper>
+      <Container>
+        <Grid container spacing={2}>
+          <Grid item container xs={4} md={6}>
+            <Brand />
+          </Grid>
+          <Grid item container xs={8} md={6} justify="flex-end">
+            <Nav />
+          </Grid>
+        </Grid>
+      </Container>
+    </HeaderWrapper>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import colors from "../../styles/settings/colors";
+
+export const HeaderWrapper = styled.header`
+  padding: var(--gap-md) 0;
+  background: ${colors.primary.color2};
+`;
